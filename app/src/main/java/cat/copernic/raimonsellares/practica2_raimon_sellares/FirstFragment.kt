28fbb90637327babb6109095d2043bdcdf5c0cc8
sellare.feiner.raimon <sellares.feiner.raimon@alumnat.copernic.cat>
2021-04-28 08:48:22 +0200

    Disseny

--- app/src/main/java/cat/copernic/raimonsellares/practica2_raimon_sellares/FirstFragment.kt
+++ app/src/main/java/cat/copernic/raimonsellares/practica2_raimon_sellares/FirstFragment.kt
@@ -6,26 +6,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.findNavController
+import cat.copernic.raimonsellares.practica2_raimon_sellares.databinding.FragmentFirstBinding
 
-/**
- * A simple [Fragment] subclass as the default destination in the navigation.
- */
 class FirstFragment : Fragment() {
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
-    }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+        val binding: FragmentFirstBinding =
+                DataBindingUtil.inflate(inflater, R.layout.fragment_first, container, false)
 
-        view.findViewById<Button>(R.id.button_first).setOnClickListener {
+        binding.buttonFirst.setOnClickListener() {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
+
+        return binding.root
     }
+
 }
